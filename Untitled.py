669349d49acf151c23400566dc211853994160da
Joaquin Saavedra <49850293+joaquinsaavedra@users.noreply.github.com>
2019-04-21 23:24:27 -0300

    Update Untitled.py

--- Untitled.py
+++ Untitled.py
@@ -1,42 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[5]:
-
-
+# In[1]:
 from pylab import *
-
-
-# In[6]:
-
-
+# In[2]:
 def H(w):
     wc = 4000*pi
     return 150 / (1.0 + 1j * w / wc)
-
-
-# In[17]:
-
-
+# In[3]:
 f = logspace(0.0,5) # frequencies from 10**0 to 10**5
-
-
-# In[ ]:
-
-
-
-
-
-# In[28]:
-
-
+# In[4]:
 plot(f, 20*log10(abs(H(2*pi*f)))); xscale('log')
 plt.xlabel ("Frecuencia (Hz)")
 plt.ylabel ("Amplitud (dB)")
 grid(color='k', linestyle='--', linewidth=1)
-
-
-
-
-
 
